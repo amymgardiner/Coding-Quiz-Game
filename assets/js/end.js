2fd3dff61e67f83a9350d36290582fed8cb18198
Amy Gardiner <amymgardiner@gmail.com>
2022-06-05 19:24:12 -0500

    added js to end page

---
+++ assets/js/end.js
@@ -0,0 +1,10 @@
+const username = document.querySelector('#username')
+const saveScoreBtn = document.querySelector('#saveScoreBtn')
+const finalScore = document.querySelector('#finalScore')
+const mostRecentScore = localStorage.getItem('mostRecentScore')
+
+const highScores = JSON.parse(localStorage.getItem('highScores')) || []
+
+const MAX_HIGH_SCORES = 5
+
+finalScore.innerText = 'All done! Your final score is ' + mostRecentScore
