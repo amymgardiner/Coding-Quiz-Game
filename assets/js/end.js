4859f6d1f7098f7046d14f5e459fb888d047d8d1
Amy Gardiner <amymgardiner@gmail.com>
2022-06-05 19:35:43 -0500

    added html for highscores page

--- assets/js/end.js
+++ assets/js/end.js
@@ -8,3 +8,27 @@ const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 const MAX_HIGH_SCORES = 5
 
 finalScore.innerText = 'All done! Your final score is ' + mostRecentScore
+
+username.addEventListener('keyup', function() {
+    saveScoreBtn.disabled = !username.value
+})
+
+function saveHighScore (e) {
+    e.preventDefault()
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value,
+    }
+
+    highScores.push(score)
+
+    highScores.sort(function(a,b) {
+        return b.score - a.score
+    })
+
+    highScores.splice(5)
+
+    localStorage.setItem('highScores', JSON.stringify(highScores))
+    window.location.assign('/')
+}
